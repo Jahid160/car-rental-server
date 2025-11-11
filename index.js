8e7ba2e946035b2c74c1af6fe9a27c36c1de7f1b
Jahid150 <jahidpcbd@gmail.com>
2025-11-11 17:23:44 +0600

    cached node_modules

--- index.js
+++ index.js
@@ -1,27 +1,40 @@
-const { MongoClient, ServerApiVersion } = require('mongodb');
-const express = require('express')
+const { MongoClient, ServerApiVersion } = require("mongodb");
+const cors = require("cors");
+const express = require("express");
+require("dotenv").config();
+const app = express();
+const port = 3000;
+app.use(cors());
+app.use(express.json());
 
-const app = express()
-const port = 3000
+const uri =
+  "mongodb+srv://RentWheels:3psr6IxnIEAAYwjp@bdpro.cwpjxwk.mongodb.net/?appName=BDPro";
 
-
-
-const uri = "mongodb+srv://RentWheels:3psr6IxnIEAAYwjp@bdpro.cwpjxwk.mongodb.net/?appName=BDPro";
-// Create a MongoClient with a MongoClientOptions object to set the Stable API version
 const client = new MongoClient(uri, {
   serverApi: {
     version: ServerApiVersion.v1,
     strict: true,
     deprecationErrors: true,
-  }
+  },
 });
+
+
 async function run() {
   try {
-    // Connect the client to the server	(optional starting in v4.7)
+    const db = client.db('RentWheels');
+    const carsCollection = db.collection("cars");
     await client.connect();
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
-    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+    console.log(
+      "Pinged your deployment. You successfully connected to MongoDB!"
+    );
+
+    app.get("/cars", async (req, res) => {
+      const cursor = carsCollection.find().sort({ created_at: -1 }).limit(6);
+      const result = await cursor.toArray();
+      res.send(result);
+    });
   } finally {
     // Ensures that the client will close when you finish/error
     // await client.close();
@@ -29,18 +42,13 @@ async function run() {
 }
 run().catch(console.dir);
 
-
-app.get('/', (req, res) => {
-  res.send('Hello World!')
-})
-
-app.get('/model', (req,res)=>{
-  res.send('model')
-})
+app.get("/", (req, res) => {
+  res.send("Hello World!");
+});
 
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
-})
+  console.log(`Example app listening on port ${port}`);
+});
 
 // 3psr6IxnIEAAYwjp
 // RentWheels
